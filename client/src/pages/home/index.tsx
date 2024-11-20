9cf956abaf9a3a713767969a1c1b2907918d8939
Itamar Shubin <itamarshubin@gmail.com>
2024-11-20 14:31:50 +0200

    remove SubmitField from home

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from '@mui/material';
-import SubmitField from '../ai/components/SubmitField';
 const Home = () => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" margin="0 20px">
@@ -10,9 +9,6 @@ const Home = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officiis dolore saepe quis quae labore nulla ab
         pariatur totam. Blanditiis architecto ratione dolor at atque unde facilis porro eaque molestias.
       </Typography>
-      <Box width={'20%'}>
-        <SubmitField questionId={5} category="AI" />
-      </Box>
     </Box>
   );
 };
